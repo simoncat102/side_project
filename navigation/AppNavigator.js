486a2dd974e774a095dda9fd27468f4e0cade725
simoncat102 <simoncat102@gmail.com>
2021-08-20 00:35:05 +0800

    0520 testing

---
+++ navigation/AppNavigator.js
@@ -0,0 +1,19 @@
+// import * as React from 'react'
+// import { NavigationContainer } from '@react-navigation/native'
+// import { createStackNavigator } from '@react-navigation/stack'
+
+// import LoginScreen from '../screen/loginView'
+
+// const Stack = createStackNavigator()
+
+// function MainStackNavigator() {
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator>
+//         <Stack.Screen name='Login' component={LoginScreen} />
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   )
+// }
+
+// export default MainStackNavigator
